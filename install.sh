527c8e9db93f8c50647076661bf71a9b850cdcda
QuentinN42 <quentin.lieumont@gmail.com>
2020-12-24 23:06:30 +0100

    Install script updated

--- install.sh
+++ install.sh
@@ -1,50 +1,26 @@
 #!/bin/bash
 
-
 function move_file() {
-    # move the file to .local/etc/git_config
-
-    mkdir -p "$HOME/.local/etc/git_config" || exit 1;
-    cp ./git_config.sh "$HOME/.local/etc/git_config/git_config.sh" || exit 2;
+  # move the file to .local/etc/git_config
+  mkdir -p "$HOME/.local/etc/git_config" || exit 1;
+  cp ./git_config.sh "$HOME/.local/etc/git_config/git_config.sh" || exit 2;
 }
 
 function make_config_folder() {
-    mkdir -p "$HOME/.config/git_config" || exit 3;
+  mkdir -p "$HOME/.config/git_config" || exit 3;
 }
 
-
-function zsh_source() {
-    # add source git_config into zshrc
-
-    echo "source \"$HOME/.local/etc/git_config/git_config.sh\"" >> "$HOME/.zshrc";
+function add_source() {
+  # source git_config into $1 if file exist and not already set
+  local line='[[ ! -f "${HOME}/.local/etc/git_config/git_config.sh" ]] || source "${HOME}/.local/etc/git_config/git_config.sh";';
+  test -f "$1" && $(grep -q "$line" $1 || echo "$line" >> $1);
 }
-
-function zsh_source_if_exist() {
-    # add source git_config into zshrc
-
-    test -f "$HOME/.zshrc" && zsh_source;
-}
-
-
-
-function bash_source() {
-    # add source git_config into zshrc
-
-    echo "source \"$HOME/.local/etc/git_config/git_config.sh\"" >> "$HOME/.zshrc";
-}
-
-function bash_source_if_exist() {
-    # add source git_config into zshrc
-
-    test -f "$HOME/.zshrc" && bash_source;
-}
-
 
 function install() {
     make_config_folder;
     move_file;
-    zsh_source_if_exist;
-    bash_source_if_exist;
+    add_source ~/.bashrc;
+    add_source ~/.zshrc;
 }
 
 install;
